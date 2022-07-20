53dd5e18084bdac2e6a821e95261bbf0197a1e27
José Milton <j1000tonm.jmb@gmail.com>
2022-07-20 15:51:48 -0300

    Home styling

---
+++ frontweb/src/components/ButtonIcon/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+const ButtonIcon = () => {
+
+    return (
+        <button className="btn btn-primary btn-icon">
+            <h6>VER CATÁLAGO</h6>
+        </button>
+    );
+}
+
+export default ButtonIcon;
